37a607914099777fd64f2b131e5acd931d8e5c8d
Oleksandr Akhobadze <oleskandr.akhobadze@student.tuke.sk>
2025-04-23 21:17:59 +0200

    fixing problem with books in schemas.py

--- schemas.py
+++ schemas.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import date
-from typing import List, Optional
+from typing import List
 
 class BookBase(BaseModel):
     title: str
@@ -27,7 +27,8 @@ class AuthorCreate(AuthorBase):
 
 class Author(AuthorBase):
     id: int
-    books: List[Book] = []
+    books: List[Book] = Field(default_factory=list)
+
 
     class Config:
         from_attributes = True
